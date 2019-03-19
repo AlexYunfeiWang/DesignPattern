6b9a4d17c5453bfe4cc3ff0296f028ebe38524cb
Valkyrie <wyfwork99@gmail.com>
2019-03-18 23:50:10 -0400

    upgraded FactoryPattern

--- FactoryPattern/src/VehicleFactory.java
+++ FactoryPattern/src/VehicleFactory.java
@@ -1,14 +1,5 @@
 public class VehicleFactory {
     public Vehicle getVehicle(VehicleType vehicleType) {
-        switch(vehicleType) {
-            case CAR:
-                return new Car();
-            case TRUCK:
-                return new Truck();
-            case BUS:
-                return new Bus();
-            default:
-                return null;
-        }
+        return vehicleType.getVehicle();
     }
 }
--- FactoryPattern/src/VehicleType.java
+++ FactoryPattern/src/VehicleType.java
@@ -1,3 +1,23 @@
 public enum VehicleType {
-    TRUCK,CAR,BUS;
+    TRUCK{
+        public Vehicle getVehicle(){
+            return new Truck();
+        }
+    },
+    CAR{
+        public Vehicle getVehicle(){
+            return new Car();
+        }
+    },
+    BUS{
+        public Vehicle getVehicle(){
+            return new Bus();
+        }
+    };
+    /*
+    this is necessary, because without it, VehicleTpe
+    won't be able to find this method, the above methods
+    are only the concrete implementation of this method
+    */
+    abstract Vehicle getVehicle();
 }
